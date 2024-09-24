a41012cb529c7a7e7b2311f5d45e91ba4f60139e
asc2563 <allistersaintclair@outlook.com>
2024-09-23 18:54:27 -0700

    modified index.js in .

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 import wisp from "wisp-server-node";
-import { createServer } from "http";
+import { createServer } from "node:http";
 
 const server = createServer();
 
 server.on("upgrade", (req, socket, head) => {
-  if (req?.url?.endsWith("/")) wisp?.routeRequest(req, socket, head);
+  if (req?.url?.endsWith("/wisp/")) wisp?.routeRequest(req, socket, head);
   else socket?.end();
 });
 
 server.on("listening", () => {
-  console.log("Wisp server is listening at http://localhost:8080");
+  console.log(`Wisp server is listening at http://localhost:8080`);
 });
 
 // https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
@@ -23,4 +23,3 @@ function shutdown() {
 }
 
 server.listen(8080);
-
