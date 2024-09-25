dd3fa7105548ef25c10c1a9018326f84d0364f51
I don’t codddde <120886940+asc2563@users.noreply.github.com>
2024-09-25 07:32:41 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,22 +4,27 @@ import { createServer } from "node:http";
 const server = createServer();
 
 server.on("upgrade", (req, socket, head) => {
-  if (req?.url?.endsWith("/wisp/")) wisp?.routeRequest(req, socket, head);
-  else socket?.end();
+  if (req.url && req.url.endsWith("/wisp/")) {
+    wisp.routeRequest(req, socket, head);
+  } else {
+    socket.destroy();
+  }
 });
 
 server.on("listening", () => {
   console.log(`Wisp server is listening at http://localhost:8080`);
 });
 
-// https://expressjs.com/en/advanced/healthcheck-graceful-shutdown.html
+// Graceful shutdown
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
 
 function shutdown() {
   console.log("SIGTERM signal received: closing HTTP server");
-  server.close();
-  process.exit(0);
+  server.close(() => {
+    console.log("HTTP server closed");
+    process.exit(0);
+  });
 }
 
 server.listen(8080);
